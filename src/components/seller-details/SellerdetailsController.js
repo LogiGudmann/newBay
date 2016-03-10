ca2662c24c9a3bcc2a3d22c72dbf4ea1ba5a785b
ingthorb@gmail.com <ingthorb@gmail.com>
2016-03-10 23:43:45 +0000

    Added all products to seller details

--- src/components/seller-details/SellerdetailsController.js
+++ src/components/seller-details/SellerdetailsController.js
@@ -6,10 +6,17 @@ angular.module("project3App").controller("SellerdetailsController",['$scope','$r
 			console.log($scope.id);
 			//TODO: get seller
 			$scope.sellerdetails = {};
+			$scope.sellerproducts = [];
 			var id = parseInt($scope.id);
 			console.log(id);
 			AppResource.getSellerDetails(id).success(function(sellerdetails){
 				$scope.sellerdetails = sellerdetails;
 			});
 			console.log($scope.sellerdetails.name);
+			AppResource.getSellerProducts(id).success(function(sellerproducts){
+					$scope.sellerproducts = sellerproducts;
+			});
+			console.log("testing");
+			console.log($scope.sellerproducts);
+
 	}]);
