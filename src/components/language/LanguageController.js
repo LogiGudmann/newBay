c4c234bcf8c63ba7050fa8967445357d78417031
ingthorb@gmail.com <ingthorb@gmail.com>
2016-03-10 01:17:34 +0000

    Added language for sellerDetail, span in translate still giving problems

--- src/components/language/LanguageController.js
+++ src/components/language/LanguageController.js
@@ -3,11 +3,12 @@
 
 angular.module("project3App").controller("LanguageController",
 	function LanguageController($scope, $translate){
+		//Default Language
 		$scope.Icelandic = function Icelandic(){
 			console.log("We go here IS");
 			$translate.use("is");
 		};
-
+		//change to English
 		$scope.English = function English(){
 			console.log("We go here EN");
 			$translate.use("en");
