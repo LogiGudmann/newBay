dadadd338d29b2fb792463e3b66183353aa0c671
ingthorb@gmail.com <ingthorb@gmail.com>
2016-03-11 01:02:40 +0000

    Added Edit button, have to create UpdateProduct function in appresource

--- src/components/seller-details/SellerdetailsController.js
+++ src/components/seller-details/SellerdetailsController.js
@@ -17,7 +17,29 @@ angular.module("project3App").controller("SellerdetailsController",
 					$scope.sellerproducts = sellerproducts;
 			});
 
+	$scope.onChange = function onChange(id){
+		//We need to create UpdateProduct function in appresource
 
+		/*console.log("ID IN onOKEDIT");
+		console.log(id);
+		AppResource.getSellerDetails(id).success(function(sellerdetails){
+					console.log("We finally go here!!!!(Getsellerdetails)");
+					$scope.sellerdetails = sellerdetails;
+				});
+		var seller = $scope.sellerdetails;
+		console.log("Testing seller name");
+		console.log(seller.name);
+		SellerDlg.show(seller).then(function(seller) {
+			console.log("we go here show dlg");
+			AppResource.updateSeller(id, seller).success(function(seller) {
+				//TODO:
+			}.error(function(){
+				centrisNotify.error("sellers.Messages.SaveFailed");
+			}));
+		});*/
+
+	};
+			
 	$scope.onAddProduct = function onAddProduct(){
   		console.log("we go here");
   		console.log("Error coming from here");
