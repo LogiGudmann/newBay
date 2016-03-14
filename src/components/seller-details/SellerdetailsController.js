00ebde496f21851009f31274ce8c491cdb896911
LogiGudmann <logi@gudmann.is>
2016-03-14 22:12:23 +0000

    Added sort function for all products

--- src/components/seller-details/SellerdetailsController.js
+++ src/components/seller-details/SellerdetailsController.js
@@ -4,6 +4,8 @@ angular.module("project3App").controller("SellerdetailsController",
 	function SellerdetailsController($scope,$routeParams,AppResource) {
 		$scope.id = $routeParams.id;
 		$scope.sellerdetails = {};
+		$scope.sortBy = 'name'; /* Default sorting by name, not category*/
+		$scope.ASCENDING = false;
 		var id = parseInt($scope.id);
 		AppResource.getSellerDetails(id).success(function(sellerdetails){
 			$scope.sellerdetails = sellerdetails;
